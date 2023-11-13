7846b22e22e7ea5fe657b397e2464951b05bce59
Bohdan Danylov <bohdandanylov@gmail.com>
2023-11-12 22:56:41 -0800

    v1.1

--- src/main/java/learnspringboot/restfulwebservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/learnspringboot/restfulwebservices/restfulwebservices/user/UserDaoService.java
@@ -11,20 +11,29 @@ import java.util.function.Predicate;
 public class UserDaoService {
     private static List<User> users = new ArrayList<>();
 
+    private static int usersCount = 0;
+
     static {
-        users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
-        users.add(new User(2, "Liam", LocalDate.now().minusYears(32)));
-        users.add(new User(3, "Noah", LocalDate.now().minusYears(34)));
-        users.add(new User(4, "Emma", LocalDate.now().minusYears(36)));
-        users.add(new User(5, "Anna", LocalDate.now().minusYears(38)));
-        users.add(new User(6, "Mila", LocalDate.now().minusYears(40)));
-        users.add(new User(7, "Aria", LocalDate.now().minusYears(42)));
-        users.add(new User(8, "Lily", LocalDate.now().minusYears(44)));
-        users.add(new User(9, "Lucy", LocalDate.now().minusYears(46)));
+        users.add(new User(++usersCount, "Adam", LocalDate.now().minusYears(30)));
+        users.add(new User(++usersCount, "Liam", LocalDate.now().minusYears(32)));
+        users.add(new User(++usersCount, "Noah", LocalDate.now().minusYears(34)));
+        users.add(new User(++usersCount, "Emma", LocalDate.now().minusYears(36)));
+        users.add(new User(++usersCount, "Anna", LocalDate.now().minusYears(38)));
+        users.add(new User(++usersCount, "Mila", LocalDate.now().minusYears(40)));
+        users.add(new User(++usersCount, "Aria", LocalDate.now().minusYears(42)));
+        users.add(new User(++usersCount, "Lily", LocalDate.now().minusYears(44)));
+        users.add(new User(++usersCount, "Lucy", LocalDate.now().minusYears(46)));
     }
 
     public List<User> findAll(){
+
         return users;
+    }
+
+    public User save(User user){
+        user.setId(++usersCount);
+        users.add(user);
+        return user;
     }
 
     public User findOne(int id){
