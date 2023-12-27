d7be23b4493df9e2c9e1337e4690ee3236cbb12d
Bohdan Danylov <bohdandanylov@gmail.com>
2023-12-27 15:18:05 -0800

    v3.40 - Added User Entity and some test data

--- src/main/java/learnspringboot/restfulwebservices/restfulwebservices/user/User.java
+++ src/main/java/learnspringboot/restfulwebservices/restfulwebservices/user/User.java
@@ -1,13 +1,19 @@
 package learnspringboot.restfulwebservices.restfulwebservices.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity(name = "user_details")
 public class User {
 
+    @Id
+    @GeneratedValue
     private Integer id;
     @Size(min=2, max=20, message = "The name must be between 2 and 20 characters.")
     @JsonProperty("user_name")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into user_details(id, birth_date, name)
+values(10001, current_date(), 'Bohdan');
+
+insert into user_details(id, birth_date, name)
+values(10002, current_date(), 'Anton');
+
+insert into user_details(id, birth_date, name)
+values(10003, current_date(), 'Anastasia');
